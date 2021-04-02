758e766223614b48a4c6242d3e54d327e466f52d
Seven Deadly Sins <xchxch1996@gmail.com>
2021-04-02 18:14:07 +0800

    coding...

--- media/src/main/java/me/magi/media/video/ADCameraManager.kt
+++ media/src/main/java/me/magi/media/video/ADCameraManager.kt
@@ -17,12 +17,10 @@ object ADCameraManager {
 
     private val manager by lazy { getApp().getSystemService(Context.CAMERA_SERVICE) as CameraManager }
     private val handler by lazy { Handler(Looper.getMainLooper()) }
-    private var mFrontCameraId: String? = null
-    private var mFrontCameraOrientation: Int? = null
-    private var mFrontCameraCharacteristics: CameraCharacteristics? = null
-    private var mBackCameraId: String? = null
-    private var mBackCameraOrientation: Int? = null
-    private var mBackCameraCharacteristics: CameraCharacteristics? = null
+    private var mFrontCameraIds = mutableListOf<String>()
+    private var mBackCameraIds = mutableListOf<String>()
+    private var characteristicsKey = hashMapOf<String, List<CameraCharacteristics.Key<*>>>()
+    // 当前正在使用的摄像头
     private var mCameraDevice: CameraDevice? = null
 
     private var mCallback: ADCameraCallback? = null
@@ -32,41 +30,32 @@ object ADCameraManager {
         Log.d(TAG, "cameraDeviceCount: ${cameraIdList.size}")
         cameraIdList.forEach { cameraId ->
             val cameraCharacteristics = manager.getCameraCharacteristics(cameraId)
-            // 获取前置摄像头
+            // 获取前置后置摄像头, 摄像头会有多个, 可以用列表保存, 方便切换
             when (cameraCharacteristics[CameraCharacteristics.LENS_FACING]) {
                 CameraCharacteristics.LENS_FACING_FRONT -> {
-                    mFrontCameraId = cameraId
-                    mFrontCameraOrientation = cameraCharacteristics[CameraCharacteristics.SENSOR_ORIENTATION]
-                    mFrontCameraCharacteristics = cameraCharacteristics
+                    mFrontCameraIds.add(cameraId)
+                    characteristicsKey[cameraId] = cameraCharacteristics.keys
                 }
                 CameraCharacteristics.LENS_FACING_BACK -> {
-                    mBackCameraId = cameraId
-                    mBackCameraOrientation = cameraCharacteristics[CameraCharacteristics.SENSOR_ORIENTATION]
-                    mFrontCameraCharacteristics = cameraCharacteristics
+                    mBackCameraIds.add(cameraId)
+                    characteristicsKey[cameraId] = cameraCharacteristics.keys
                 }
             }
-
-
-            if (cameraCharacteristics[CameraCharacteristics.LENS_FACING] == CameraCharacteristics.LENS_FACING_BACK) {
-                mBackCameraId = cameraId
-                cameraCharacteristics[CameraCharacteristics.SENSOR_ORIENTATION]
-                mBackCameraCharacteristics = cameraCharacteristics
-            } else if (cameraCharacteristics[CameraCharacteristics.LENS_FACING] == CameraCharacteristics.LENS_FACING_FRONT) {
-                mFrontCameraId = cameraId
-                mFrontCameraCharacteristics = cameraCharacteristics
-            }
         }
+    }
+
+    private fun getCameraOrientation(cameraId: String): Int {
+        return manager.getCameraCharacteristics(cameraId).get(CameraCharacteristics.SENSOR_ORIENTATION)?:-1
     }
 
     @SuppressLint("MissingPermission")
     fun openCamera() {
         // 优先开启后置摄像头
-        val cameraId = mBackCameraId ?: mFrontCameraId
-        if (cameraId != null) {
-            manager.openCamera(cameraId, CameraStateCallback(), handler)
-        } else {
+        if (mBackCameraIds.isEmpty() && mFrontCameraIds.isEmpty()) {
             throw RuntimeException("没有找到可用的摄像头")
         }
+        val cameraId = mBackCameraIds.getOrNull(0) ?: mFrontCameraIds[0]
+        manager.openCamera(cameraId, CameraStateCallback(), handler)
     }
 
     fun closeCamera() {
